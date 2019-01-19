faac689132ac2cad2e70ee65c5c297934ba33cdb
Priit Puru <priit.puru@twn.ee>
2019-01-20 01:20:10 +0200

    Changes

--- gallery.js
+++ gallery.js
@@ -2,23 +2,23 @@ const imageData = [
     // lets pretend this is from an API
     {
         file: 'https://cdn.pixabay.com/photo/2017/07/17/23/51/wormhole-2514312_1280.jpg',
-        title: ''
+        title: 'Wormhole'
     },
     {
         file: 'https://cdn.pixabay.com/photo/2018/04/06/11/21/office-3295556_1280.jpg',
-        title: ''
+        title: 'Lorem ipsum dolor sit a met'
     },
     {
         file: 'https://cdn.pixabay.com/photo/2017/10/17/19/11/fantasy-2861815_1280.jpg',
-        title: ''
+        title: 'Eritipikkjasuurtohutultvõimassõna mis võiks breakida normaalselt'
     },
     {
         file: 'https://cdn.pixabay.com/photo/2016/03/26/13/09/cup-of-coffee-1280537_1280.jpg',
-        title: ''
+        title: 'Cup of coffee'
     },
     {
         file: 'https://cdn.pixabay.com/photo/2017/01/06/13/50/smartphone-1957740_960_720.jpg',
-        title: ''
+        title: 'Man holding smartphone'
     },
     
 ];
@@ -63,38 +63,36 @@ class Gallery {
     }
     generateImages(galleryElement, imageTemplate, images) {
         const gallery = qs(galleryElement);
-        images.map(
+        images.forEach(
             image => {
                 const template = qs(imageTemplate).content.cloneNode(true);
                 const templateInner = this.getNodeChildWithClass(template, 'Image');
                 templateInner.addEventListener('click', () => { 
-                    this.openModal(image.file)}
+                    this.openModal(image)}
                 );
                 const templateImage = this.getNodeChildWithNodeName(templateInner, 'IMG');
                 templateImage.setAttribute('src', image.file);
+                templateImage.setAttribute('alt', image.title);
                 gallery.appendChild(template);
             }
         );
     }
-    openModal(file) {
+    openModal(image) {
         const imageModal = qs('.Modal--image');
+        const imageModalHeader = this.getNodeChildWithClass(imageModal, 'Modal__footer');
+        this.getNodeChildWithNodeName(imageModalHeader, 'H3').innerText = image.title;
         const imageModalInner = this.getNodeChildWithClass(imageModal, 'Modal__inner');
-        console.log(imageModalInner.childNodes);
         const imageModalImage = this.getNodeChildWithNodeName(imageModalInner, 'IMG');
-        imageModalImage.setAttribute('src', file);
+        imageModalImage.setAttribute('src', image.file);
+        imageModalImage.setAttribute('alt', image.title);
         imageModal.showModal();
     }
-    closeModal() {
-
-    }
     getNodeChildWithClass(node, className) {
-        console.log(node, className);
         return Array.from(node.childNodes).filter(
             el => el.className ? el.className.match(className) : false
         )[0];
     }
     getNodeChildWithNodeName(node, nodeName) {
-        console.log(node, nodeName);
         return Array.from(node.childNodes).filter(
             el => el.nodeName === nodeName
         )[0];
